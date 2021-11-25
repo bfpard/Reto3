cd1f10e6d0c98ab46c515978d86857f2baa329b5
Brayan Felipe Pardo Cortes <felcor1996@outlook.com>
2021-11-25 12:54:12 -0500

    Configuracion base del front y el back

---
+++ Backend/Api/src/main/java/Reto3/Api/ApiAngularApplication.java
@@ -0,0 +1,13 @@
+package Reto3.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAngularApplication.class, args);
+	}
+
+}
---
+++ Backend/Api/src/test/java/Reto3/Api/ApiAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package Reto3.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
